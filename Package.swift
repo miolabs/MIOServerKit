bfc134312649d68d00bb94de74ad8c0e5cd0e3c4
Javier Segura <javier@miolabs.com>
2023-07-25 13:26:55 +0200

    going back to brnaches

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "MIOServerKit",
-    platforms: [
-        .macOS(.v10_15)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -22,9 +19,6 @@ let package = Package(
         .package(url: "https://github.com/Kitura/HeliumLogger.git", from: "1.9.200"),
         .package(url: "https://github.com/Kitura/Kitura-CORS.git", from: "2.1.201"),
         .package(url: "https://github.com/miolabs/MIOCore.git", .branch("master")),
-        // .package(url: "https://github.com/miolabs/MIOCoreData.git", .branch("master")),
-        // .package(url: "https://github.com/miolabs/MIODB.git", .branch("master")),
-        // .package(url: "https://github.com/miolabs/MIOPersistentStore.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
