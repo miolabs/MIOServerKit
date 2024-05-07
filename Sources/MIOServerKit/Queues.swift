328d0dbc718d96561e10fff7bc8123ed242134b1
Javier Segura <javier@miolabs.com>
2024-05-07 07:30:01 +0200

    adding checking support for a server queue

--- Sources/MIOServerKit/Queues.swift
+++ Sources/MIOServerKit/Queues.swift
@@ -8,6 +8,16 @@
 import Foundation
 import MIOCore
 
+let _server_queue_prefix = "com.miolabs.server"
+
 public func sync_queue_id ( _ key: String ) -> DispatchQueue {
-    return MIOCoreQueue( label: key, prefix: "com.miolabs.server" )
+    return MIOCoreQueue( label: key, prefix: _server_queue_prefix )
+}
+
+public func sync_queue_status ( _ key: String ) -> Bool {
+    return MIOCoreQueueStatus( label: key, prefix: _server_queue_prefix )
+}
+
+public func sync_queues_set_status ( _ value:Bool, _ key: String ) {
+    MIOCoreQueueSetStatus( value: value, label: key, prefix: _server_queue_prefix )
 }
