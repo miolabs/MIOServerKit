c29c3c4a16195b62c4a74281542a44140a935717
Javier Segura <javier@miolabs.com>
2024-08-08 11:35:33 +0200

    adding logger

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MIOServerKit",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library( name: "MIOServerKit", targets: ["MIOServerKit"] ),
--- Sources/MIOServerKit-NIO/Server+NIO.swift
+++ Sources/MIOServerKit-NIO/Server+NIO.swift
@@ -11,6 +11,9 @@ import Foundation
 import NIO
 import NIOHTTP1
 import MIOServerKit
+import os
+
+let _logger = Logger()
 
 open class NIOServer : Server
 {        
@@ -42,15 +45,17 @@ open class NIOServer : Server
             let channel = try bootstrap.bind( host: "0.0.0.0", port: port ).wait()
 
             guard let channelLocalAddress = channel.localAddress else {
+                _logger.fault("Address was unable to bind. Please check that the socket was not closed or that the address family was understood.")
                 fatalError("Address was unable to bind. Please check that the socket was not closed or that the address family was understood.")
-               }
+            }
             
-            print("Server started and listening on \(channelLocalAddress), htdocs path \(documentsPath)")
+            _logger.info( "Server started and listening on \(channelLocalAddress), htdocs path \(self.documentsPath)")
 
             // This will never unblock as we don't close the ServerChannel
             try channel.closeFuture.wait()
         } catch {
-            
+            _logger.fault("\(error)")
+            fatalError("\(error)")
         }
     }
     
